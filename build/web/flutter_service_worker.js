91c1d52533128d9e0c19ec01f1a5ff1edd3483c3
JEYBAN37 <jeyban37@gmail.com>
2025-09-24 20:59:23 -0500

    jeysgegu deploy: feacture

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "081a5661418d781e770e485659de7597
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "deef51213d01a3071e9a8ea706839fad",
+"flutter_bootstrap.js": "39b98fa111fa2ad11342d1bbcf65bb90",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
