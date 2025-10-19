6fd4936347f49f6bd64e8032fb324466edb5be5a
JEYBAN37 <jeyban37@gmail.com>
2025-10-19 17:54:58 -0500

    jeysgegu refactor: carpétas nuevas

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1978094036"
+    serviceWorkerVersion: "3650772256"
   }
 });
