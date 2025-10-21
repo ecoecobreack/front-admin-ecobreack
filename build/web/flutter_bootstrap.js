21d393610dad04494c02c1db9a2e588b1dda0363
JEYBAN37 <jeyban37@gmail.com>
2025-10-21 00:48:36 -0500

    jeysgegu refactor: errores de render

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "408090702"
+    serviceWorkerVersion: "946820745"
   }
 });
