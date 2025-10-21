e4eed7519d196b7055034e5b4ca9f19375176a7b
JEYBAN37 <jeyban37@gmail.com>
2025-10-21 00:05:30 -0500

    jeysgegu refactor: fornt funcional

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3211348731"
+    serviceWorkerVersion: "2312754533"
   }
 });
